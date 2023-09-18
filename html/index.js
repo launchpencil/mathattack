ccf7b7a523e7df2ec80d54d4fb67c7c908606d79
launchpencil <launchpencil@gmail.com>
2023-09-18 17:08:48 +0900

    でけた

---
+++ html/index.js
@@ -0,0 +1,39 @@
+var question = [
+    ['sin0', 'sin180', 'cos90', 'tan0', 'tan180'],//0
+    ['tan30', 'tan150'],//r3 1
+    ['tan45', 'tan135', 'sin90', 'cos0', 'cos180'],//1
+    ['tan60', 'tan120'],//r3
+    ['sin60', 'sin120','cos30', 'cos150'],//2 r3
+    ['sin45', 'sin135','cos45', 'cos135'],//2 r2
+    ['sin30', 'sin150','cos60', 'cos120'],//2 1
+    ['tan90']//none
+]
+
+var answer = 5
+var score = 0
+
+function don() {
+    answer = Math.floor(Math.random() * 8);
+
+    console.log('answer: ' + answer);
+
+    document.getElementById('q').innerText = question[answer][Math.floor(Math.random() * question[answer].length)] + '°';
+}
+
+don();
+
+document.querySelectorAll('.select').forEach(function(button) {
+
+    button.addEventListener('click', function (event) {
+        var clickedElementId = event.currentTarget.id;
+
+        console.log(clickedElementId);
+
+        if (clickedElementId == answer) {
+            score++;
+            don();
+        } else {
+            alert("ゲームオーバー\nスコア：" + score)
+        }
+    });
+});
